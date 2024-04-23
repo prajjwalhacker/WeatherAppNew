e673c74f436cbf164a05f8dadd6cc302fc523696
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-04-23 15:28:12 +0530

    weather input added

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,20 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import { useState, useRef, useEffect } from "react";
 
 export default function Home() {
+  const [city, setCity] = useState('')
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+     inputRef.current.focus();
+  }, [])
+
   return (
     <main className={styles.main}>
-       <div>
-         dfgfdgkjdfgdfgdfg
-         dfgdfgdfjkgdfgjdf
-         fdgdfgdfklg
-       </div>
+        <div className={styles.weatherContainer}>
+            <input type="text" onChange={(e) => { setCity(e.target.value) }} value={city} ref={inputRef}/>
+        </div>
     </main>
   );
 }
